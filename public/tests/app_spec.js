bfd370e491ec8923683e8be5b41eb28f820fcfd7
FORNO <forno@xmaho.link>
2022-10-13 14:26:07 +0900

    Add test for problemView with call 42
    Signed-off-by: FORNO <forno@xmaho.link>

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -2,10 +2,16 @@ describe('LearnJS', () => {
   it('can showa problem view', () => {
     learnjs.showView('#problem-1');
     expect(document.querySelectorAll('.view-container .problem-view').length).toEqual(1);
-  })
+  });
 
   it('shows the landing page view when there is no hash', () => {
     learnjs.showView('');
     expect(document.querySelectorAll('.view-container .landing-view').length).toEqual(1);
-  })
-})
+  });
+
+  it('passes the hash view parameter to the view function', () => {
+    spyOn(learnjs, 'problemView');
+    learnjs.showView('#problem-42');
+    expect(learnjs.problemView).toHaveBeenCalledWith('42');
+  });
+});
