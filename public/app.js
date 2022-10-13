545cf325698b686eedbf4b92cefee5d7586266ea
FORNO <forno@xmaho.link>
2022-10-13 17:35:19 +0900

    Add data-binding without test updation
    Signed-off-by: FORNO <forno@xmaho.link>

--- public/app.js
+++ public/app.js
@@ -6,9 +6,27 @@ learnjs.removeAllChildren = (element) => {
   }
 }
 
+learnjs.problems = [
+  {
+    description: 'What is truth?',
+    code: 'function problem() { return __; }'
+  },
+  {
+    description: 'Simple Math',
+    code: 'function problem() { return 42 ==== 6 * __; }'
+  },
+]
+
+learnjs.applyObject = (obj, elem) => {
+  for (const key in obj) {
+    elem.querySelector(`[data-name="${key}"]`).textContent = obj[key]
+  }
+}
+
 learnjs.problemView = (problemNumber) => {
   const problemView = document.querySelector('.templates .problem-view').cloneNode(true);
-  problemView.querySelector('.title').textContent = `Problem #${problemNumber} Coming soon!`;
+  problemView.querySelector('.title').textContent = `Problem #${problemNumber}`;
+  learnjs.applyObject(learnjs.problems[problemNumber - 1], problemView)
   return problemView;
 }
 
