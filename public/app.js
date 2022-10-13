a753000cd37e16e27984214a18560a7c5bb8bbfe
FORNO <forno@xmaho.link>
2022-10-13 14:12:40 +0900

    Add routes function
    Signed-off-by: FORNO <forno@xmaho.link>

--- public/app.js
+++ public/app.js
@@ -1,9 +1,26 @@
 'use strict';
 const learnjs = {}
-learnjs.showView = (hash) => {
+learnjs.removeAllChildren = (element) => {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild)
+  }
+}
+
+learnjs.problemView = () => {
   const problemView = document.createElement('div');
   problemView.textContent = 'Coming soon!';
   problemView.classList.add('problem-view');
+  return problemView;
+}
 
-  document.getElementsByClassName('view-container')[0]?.append(problemView);
+learnjs.showView = (hash) => {
+  const routes = {
+    '#problem-1': learnjs.problemView
+  }
+  const viewFn = routes[hash];
+  if (viewFn) {
+    const container = document.querySelector('.view-container');
+    learnjs.removeAllChildren(container);
+    container.append(viewFn());
+  }
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -3,4 +3,9 @@ describe('LearnJS', () => {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
   })
+
+  it('shows the landing page view when there is no hash', () => {
+    learnjs.showView('');
+    expect(document.querySelectorAll('.view-container .landing-view').length).toEqual(1);
+  })
 })
